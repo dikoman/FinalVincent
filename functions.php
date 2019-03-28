0274adcdade805419b038fcbe4c47130e4789923
Vincent Gagnon <dikoman123@hotmail.com>
2019-03-28 15:11:05 -0400

    tp2 commit 1

--- functions.php
+++ functions.php
@@ -8,9 +8,9 @@ function my_theme_enqueue_styles() {
 
     wp_enqueue_style(
         'twentynineteen-style-enfant',
-        get_stylesheet_directory_uri() . '/style.css',
+        get_stylesheet_directory_uri() . '/css/style.css',
         array(),
-        filemtime( get_stylesheet_directory() . '/style.css' )
+        filemtime( get_stylesheet_directory() . '/css/style.css' )
     );
 
   
